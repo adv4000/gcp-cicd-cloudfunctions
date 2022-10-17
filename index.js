15e106d2a63a4f4b745e74278825a792f396776c
Ivan Fanchenko <103429187+voyager1122@users.noreply.github.com>
2022-10-17 14:49:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.2</b>";
+  const message="<font color='blue'>СloudFunction test!</font><br><b>App Version 1.3</b>";
   res.status(200).send(message);
 };
